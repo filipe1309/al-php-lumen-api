fb0087f133b242124f6c4524a80ebe074e19db49
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-09 14:27:15 -0300

    Update SeriesController index return

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class SeriesController extends BaseController
 {
     public function index()
     {
-        return [
-            'Breaking Bad',
-            'The Office'
-        ];
+        return Serie::all();
     }
 }

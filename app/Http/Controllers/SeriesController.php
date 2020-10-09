28f8d0bd2c00abd749ea81e9a07225042ad55739
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-09 19:17:15 -0300

    Add Read Serie

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -18,4 +18,15 @@ class SeriesController extends BaseController
     {
         return response()->json(Serie::create(['nome' => $request->nome]), Response::HTTP_CREATED);
     }
+
+    public function get(int $id)
+    {
+        $serie = Serie::find($id);
+
+        if (is_null($serie)) {
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return response()->json($serie);
+    }
 }

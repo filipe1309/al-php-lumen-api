c8a7266341feccd0b65ea75ce5115ca250c6391e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-09 18:45:21 -0300

    Add Store Serie

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class SeriesController extends BaseController
@@ -10,5 +12,10 @@ class SeriesController extends BaseController
     public function index()
     {
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json(Serie::create(['nome' => $request->nome]), Response::HTTP_CREATED);
     }
 }

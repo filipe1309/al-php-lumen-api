bb0e7556b81e0b66aeea48eeb979fd48f4800fda
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-10 20:36:12 -0300

    Replace AuthServiceProvider GenericUser with real User

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -42,8 +42,8 @@ class AuthServiceProvider extends ServiceProvider
             $token = str_replace('Bearer ', '', $authorizationHeader);
             $dadosAutenticacao = JWT::decode($token, env('JWT_KEY'), ['HS256']);
 
-            return new GenericUser(['email' => $dadosAutenticacao]);
-            // return User::where('email', $dadosAutenticacao['email'])->first();
+            // return new GenericUser(['email' => $dadosAutenticacao]);
+            return User::where('email', $dadosAutenticacao->email)->first();
         });
     }
 }

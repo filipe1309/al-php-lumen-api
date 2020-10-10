ac610c5b3633365a543d1e9f0f9e42b0eaac40e9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-10 17:14:50 -0300

    Paginate buscaPorSerie

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -14,7 +14,7 @@ class EpisodiosController extends BaseController
 
     public function buscaPorSerie(int $serieId)
     {
-        $episodios = Episodio::query()->where('serie_id', $serieId)->get();
+        $episodios = Episodio::query()->where('serie_id', $serieId)->paginate();
         return $episodios;
     }
 }

c05c2cabe8bc5d1dd74320ddcaa7528173210302
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-10 17:19:30 -0300

    Add meta info with HATEOAS

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -9,10 +9,19 @@ class Serie extends Model
 {
     public $timestamps = false;
     protected $fillable = ['nome'];
+    protected $appends = ['links'];
     // protected $perPage = 3; // default 15
 
     public function episodios()
     {
         return $this->hasMany(Episodio::class);
+    }
+
+    public function getLinksAttribute($links): array
+    {
+        return [
+            'self' => '/api/series/' . $this->id,
+            'episodios' => '/api/series/' . $this->id . '/episodios'
+        ];
     }
 }

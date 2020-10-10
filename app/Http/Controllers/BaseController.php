7aaa346422952f5e3f77cdeb2faf62497242f972
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-10 12:14:47 -0300

    Create a BaseController

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+abstract class BaseController
+{
+    protected string $classe;
+
+    public function index()
+    {
+        return $this->classe::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json($this->classe::create($request->all()), Response::HTTP_CREATED);
+    }
+
+    public function show(int $id)
+    {
+        $recurso = $this->classe::find($id);
+
+        if (is_null($recurso)) {
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return response()->json($recurso);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $recurso = $this->classe::find($id);
+        if (is_null($recurso)) {
+            return response()->json(['error' => 'Recurso nao encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        $recurso->fill($request->all());
+        $recurso->save();
+
+        return response()->json($recurso);
+    }
+
+    public function destroy(int $id)
+    {
+        $qtdRecursosRemovidas = $this->classe::destroy($id);
+        if ($qtdRecursosRemovidas === 0) {
+            return response()->json(['error' => 'Recurso nao encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,53 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Laravel\Lumen\Routing\Controller as BaseController;
+// use Laravel\Lumen\Routing\Controller as BaseController;
 
 class SeriesController extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return Serie::all();
-    }
-
-    public function store(Request $request)
-    {
-        return response()->json(Serie::create($request->all()), Response::HTTP_CREATED);
-    }
-
-    public function show(int $id)
-    {
-        $serie = Serie::find($id);
-
-        if (is_null($serie)) {
-            return response()->json(null, Response::HTTP_NO_CONTENT);
-        }
-
-        return response()->json($serie);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Serie::find($id);
-        if (is_null($serie)) {
-            return response()->json(['error' => 'Recurso nao encontrado'], Response::HTTP_NOT_FOUND);
-        }
-
-        $serie->fill($request->all());
-        $serie->save();
-
-        return response()->json($serie);
-    }
-
-    public function destroy(int $id)
-    {
-        $qtdRecursosRemovidas = Serie::destroy($id);
-        if ($qtdRecursosRemovidas === 0) {
-            return response()->json(['error' => 'Recurso nao encontrado'], Response::HTTP_NOT_FOUND);
-        }
-
-        return response()->json(null, Response::HTTP_NO_CONTENT);
+        $this->classe = Serie::class;
     }
 }
